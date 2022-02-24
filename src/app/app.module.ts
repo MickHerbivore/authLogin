80bb8412088d52b5aeafa36b1632f4592deff772
Miguel Muñoz Caro <mick.munozcaro@gmail.com>
2022-02-23 21:32:33 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-w7nn53is.us.auth0.com',
+      clientId: 'vxElN6FhcyH3yVESviojk6diYmR2fi4J'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
